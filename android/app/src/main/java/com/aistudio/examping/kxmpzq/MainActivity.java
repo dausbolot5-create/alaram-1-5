779199083637c3b26533c46e595d209c6beb6836
dausbolot5-create <dausbolot5@github>
2026-07-28 19:21:28 +0700

    build: setup Capacitor for seamless web-to-apk build

---
+++ android/app/src/main/java/com/aistudio/examping/kxmpzq/MainActivity.java
@@ -0,0 +1,5 @@
+package com.aistudio.examping.kxmpzq;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.aistudio.examping.kxmpzq',
+  appName: 'Examping',
+  webDir: 'dist'
+};
+
+export default config;
